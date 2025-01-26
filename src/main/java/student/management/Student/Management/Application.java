2e2bfede50f0359c78b68f0ce2ec21c6887619ad
Hiroishi Ryuta <rh.ryuta0712@gmail.com>
2025-01-26 14:17:43 +0900

    3度目のコミット

--- src/main/java/student/management/Student/Management/Application.java
+++ src/main/java/student/management/Student/Management/Application.java
@@ -1,14 +1,23 @@
 package student.management.Student.Management;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@Transactional
+
 public class Application {
+  @Autowired
+  private StudentRepository repository;
 
 	private String name = "Hiroishi Ryuta";
 	private String age = "24";
@@ -19,20 +28,29 @@ public class Application {
 	}
 
 	//検索エンジンに追加入力するアドレスのようなもの
- @GetMapping("/StudentInfo")
- public String getStudentInfo() {
-
+ @GetMapping("/Student")
+ public String getStudent(@RequestParam String name) {
+	 System.out.println("検索する名前: " + name);//デバック用
+		Student student = repository.searchByName(name);
+	 if (student == null) {
+		 return "Student not found";
+	 }
 		//実行結果　画面に出力されるもの
-		return name + "さん" + age + "歳";
+		return student.getName() + " " + student.getAge() + "歳";
  }
-@PostMapping("/StudentInfo")
-	public void setStudentInfo(String name, String age){
-		this.name = name;
-		this.age = age;
+@PostMapping("/Student")
+	public void registerStudent(String name, int age){
+		repository.registerStudent(name,age);
  }
 
- @PostMapping("StudentName")
-	public void updateStudentName(String name){
-		this.name = name;
- }
+ @PatchMapping ("/Student")
+	public void updateStudent(String name,int age){
+		repository.updateStudent(name,age);
+	}
+
+@DeleteMapping ("/Student")
+	public void  deleteStudent(String name){
+		repository.deleteStudent(name);
+  }
 }
+
