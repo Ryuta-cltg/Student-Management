b2b1c5ef6c6e360d7ddf76c3c238053ba6605642
Hiroishi Ryuta <rh.ryuta0712@gmail.com>
2025-07-27 16:18:00 +0900

    Converter Testクラスの作成
    処理実装前

---
+++ src/test/java/student/management/Student/Management/controller/converter/StudentConverterTest.java
@@ -0,0 +1,12 @@
+package student.management.Student.Management.controller.converter;
+
+import org.junit.jupiter.api.Test;
+
+class StudentConverterTest {
+  private final StudentConverter converter = new StudentConverter();
+
+  @Test
+  void convertStudentDetails_正しくマッピングされること(){
+
+  }
+}
